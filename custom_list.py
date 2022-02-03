85fd640b54fe6def1ee02de134d92d3b05a04b4c
unknown <kirill.gubanov21@gmail.com>
2022-02-03 20:26:58 +0100

    Add custom list file with CustomList class for creating chess board

---
+++ custom_list.py
@@ -0,0 +1,14 @@
+class CustomList(list):
+    """ Custom list with throwing an exception, when index going beyond the list (without -1, -2 ... indexes )"""
+
+    def __getitem__(self, index):
+        if index < 0:
+            raise IndexError(f'Expected a positive index, instead got {index}')
+
+        return super(CustomList, self).__getitem__(index)
+
+    def __setitem__(self, key, value):
+        if key < 0:
+            raise IndexError(f'Expected a positive index, instead got {key}')
+
+        return super(CustomList, self).__setitem__(key, value)
