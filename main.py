37bcada7bbba1e040e592f79c7b118b6c00066ed
unknown <kirill.gubanov21@gmail.com>
2022-02-03 22:14:18 +0100

    Create view for rendering url type /api/v1/{chess_figure}/{current_field}

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
-from chess import ChessBoard, King, Bishop, Rook, Pawn, Knight, Queen
+import chess
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1/<chess_figure>/<current_field>")
+def index(chess_figure, current_field):
+    current_field = (
+        current_field.capitalize()
+    )  # for passing to function of ChessBoard class
+    board = chess.ChessBoard()
+
+    try:
+        figure_class = getattr(chess, chess_figure.capitalize())
+        figure = figure_class(board, current_field)
+    except AttributeError:
+        return jsonify(
+            availableMoves=[],
+            error="No figure with such name",
+            figure=None,
+            current_field=current_field,
+        )
+
+    if not board.check_if_field_exists(current_field):
+        return jsonify(
+            availableMoves=[],
+            error="No such field on the board",
+            figure=type(figure).__name__,
+            current_field=current_field,
+        )
+
+    return jsonify(
+        availableMoves=figure.list_available_moves(),
+        error=None,
+        figure=type(figure).__name__,
+        current_field=current_field,
+    )
+
 
 if __name__ == "__main__":
-    board = ChessBoard()
-    board.show_board()
-    king = King(board, "H2")
-    rook = Rook(board, "H1")
-    bishop = Bishop(board, "E4")
-    knight = Knight(board, "E4")
-    pawn = Pawn(board, "C2")
-    queen = Queen(board, "E4")
-
-    print("\n")
-    print(queen.list_available_moves())
+    app.run()
